63c771da306599f65ee80e9d80ce9ea277063dd9
MeryemDH <maryamdhouib4523@gmail.com>
2025-05-11 17:34:01 +0100

    'version-sans-nodeenv'

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express(); 
-const __dirname = path.resolve();
 
 
 app.use(cors());
@@ -17,13 +16,15 @@ app.use(express.json()); // allows us to accept JSON data in the req.body
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("/", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("/", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 	
 app.listen(PORT, () => {
